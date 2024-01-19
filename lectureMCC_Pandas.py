617ff721d171fddf7b0a6c7f43e8a54fed63d717
Julien <j-laffarguette@o-lambret.fr>
2024-01-19 13:22:49 +0100

    Update lectureMCC_Pandas.py
    modifs JL

--- lectureMCC_Pandas.py
+++ lectureMCC_Pandas.py
@@ -1,13 +1,16 @@
 import pandas as pd
 import re
 
+
 class LectureMccCuve():
     def __init__(self, mccFile):
+        self.collOffsetCrossPlane = None
+        self.collOffsetInPlane = None
         self.mccFile = mccFile
         self.positions = []
         self.values = []
         self.listeCle = []
-        #dataframes = []
+        # dataframes = []
         self.line_in_measurement = False
         self.depth = None
         self.key = None
@@ -16,7 +19,6 @@ class LectureMccCuve():
         self.fieldCrossPlane = None
         self.dictMCC = {}
         print('self.mccFile : ', self.mccFile)
-
 
         self.getValues(self.mccFile)
         self.listeCle = list(self.dictMCC.keys())
@@ -30,28 +32,44 @@ class LectureMccCuve():
                 searchFieldCrossPlane = re.search("^\t\tFIELD_CROSSPLANE=(\d+\.\d+)$", line_striped)
                 searchDepth = re.search("SCAN_DEPTH=(\d+\.\d+)", line_striped)
                 searchSSD = re.search('SSD=(\d+\.\d+)', line_striped)
+                searchCOLL_OFFSET_INPLANE = re.search("COLL_OFFSET_INPLANE=(-?\d+\.\d+)", line_striped)
+                searchCOLL_OFFSET_CROSSPLANE = re.search("COLL_OFFSET_CROSSPLANE=(-?\d+\.\d+)", line_striped)
+                # recherche SCAN_OFFAXIS_INPLANE
+                searchOffAxisInPlane = re.search("SCAN_OFFAXIS_INPLANE=(-?\d+\.\d+)", line_striped)
 
-                if re.search("SCAN_CURVETYPE=(\w+)", line_striped):
+                if searchScanCurve:
                     self.scanCurve = searchScanCurve.group(1)
-
-                if re.search("^\t\tFIELD_INPLANE=(\d+\.\d+)$", line_striped):
+                if searchFieldInPlane:
                     self.fieldInPlane = searchFieldInPlane.group(1)
-
-                if re.search("^\t\tFIELD_CROSSPLANE=(\d+\.\d+)$", line_striped):
+                if searchFieldCrossPlane:
                     self.fieldCrossPlane = searchFieldCrossPlane.group(1)
-
-                if re.search("SCAN_DEPTH=(\d+\.\d+)", line_striped):
+                if searchDepth:
                     self.depth = searchDepth.group(1)
-
-                if re.search('SSD=(\d+\.\d+)', line_striped):
+                if searchSSD:
                     self.SSD = searchSSD.group(1)
+                if searchOffAxisInPlane:
+                    self.offAxisInPlane = searchOffAxisInPlane.group(1)
+                if searchCOLL_OFFSET_INPLANE:
+                    self.collOffsetInPlane = searchCOLL_OFFSET_INPLANE.group(1)
+                if searchCOLL_OFFSET_CROSSPLANE:
+                    self.collOffsetCrossPlane = searchCOLL_OFFSET_CROSSPLANE.group(1)
 
                 if "BEGIN_DATA" in line_striped:
                     self.line_in_measurement = True
                     if self.depth:
-                        self.key = str(self.scanCurve) + str(' Field Size = ') + str(self.fieldInPlane) + str(' ') + str(self.fieldCrossPlane) + str(' , depth = ') + str(self.depth) + str(' , SSD = ') + str(self.SSD)
+                        self.key = str(self.scanCurve) + str(' Field Size = ') + str(self.fieldInPlane) + str(' ') \
+                                   + str(self.fieldCrossPlane) + str(' , depth = ') + str(self.depth) + str(' , SSD = ') \
+                                   + str(self.SSD) + str(' , offAxisInPlane = ') + str(self.offAxisInPlane) + str(
+                            ' , collOffsetInPlane = ') \
+                                   + str(self.collOffsetInPlane) + str(' , collOffsetCrossPlane = ') + str(
+                            self.collOffsetCrossPlane)
+
                     else:
-                        self.key = str(self.scanCurve) + str(' Field Size = ') + str(self.fieldInPlane) + str(' ') + str(self.fieldCrossPlane) + str(' , SSD = ') + str(self.SSD)
+                        self.key = str(self.scanCurve) + str(' Field Size = ') + str(self.fieldInPlane) + str(' ') \
+                                   + str(self.fieldCrossPlane) + str(' , SSD = ') + str(self.SSD) \
+                                   + str(' , offAxisInPlane = ') + str(self.offAxisInPlane) \
+                                   + str(' , collOffsetInPlane = ') + str(self.collOffsetInPlane) \
+                                   + str(' , collOffsetCrossPlane = ') + str(self.collOffsetCrossPlane)
 
                 elif "END_DATA" in line_striped:
                     data = {'position': self.positions, 'value': self.values}
@@ -66,7 +84,6 @@ class LectureMccCuve():
                 elif self.line_in_measurement:
                     self.positions.append(line.split()[0])
                     self.values.append(line.split()[1])
-
 
     def printDict(self):
         print('dict : ', self.dictMCC)
